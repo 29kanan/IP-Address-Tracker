983f1c3dbadd4fd12dcb443abaaae90dccfda5e8
kanan <kantalreja48@gmail.com>
2025-12-04 08:42:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./myscreenshot1.png)
+
+
+### Links
+
+- Solution URL: [https://github.com/29kanan/IP-Address-Tracker.git](https://github.com/29kanan/IP-Address-Tracker.git)
+- Live Site URL: [https://695e5fffebd1c66726b05a32--my-ip-address-tracker.netlify.app/](https://695e5fffebd1c66726b05a32--my-ip-address-tracker.netlify.app/)
+
+## My process
+
+### Built with
+
+- HTML/CSS
+- JavaScript
+- Leaflet.js (Map Library)
+- Ipify API
+
+### What I learned
+
+- API Integration: I learned how to use fetch() to retrieve location data from the Ipify API based on user input.
+- Leaflet.js Basics: I learned how to initialize map and place a custom marker at specific coordinates ($latitude, longitude$).
+
+
+### Continued development
+
+- Multi-IP Comparison: Users will be allowed to search for several IPs and see them all on the map at the same time to compare distances.
+- Map Customization: I am planning to add a 'Theme Switcher'where users can choose between different map styles, like "Dark Mode" or "Satellite View".
+
---
+++ script.js
@@ -0,0 +1,61 @@
+const searchInp = document.getElementById("search-inp");
+const searchBtn = document.getElementById("search-btn");
+
+const ipDisplay = document.getElementById("ip-add");
+const locDisplay = document.getElementById("loc");
+const timeDisplay = document.getElementById("timezone");
+const ispDisplay = document.getElementById("isp");
+
+const API_KEY = 'at_2NeIy41ObMgFo1JNg1tBGEIcbAvzn'; 
+const BASE_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
+
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18
+}).addTo(map);
+const myIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+    iconSize: [46, 56],
+    iconAnchor: [23, 56]
+});
+let marker = L.marker([51.505, -0.09], {icon: myIcon}).addTo(map);
+
+async function searchIP(ipAddress){
+    try{
+        const res = await fetch(`${BASE_URL}&ipAddress=${ipAddress}`);
+        const data = await res.json();
+        
+        ipDisplay.innerText = '';
+        locDisplay.innerText = '';
+        timeDisplay.innerText = '';
+        ispDisplay.innerText = '';
+
+        ipDisplay.innerText = data.ip;
+        locDisplay.innerText = `${data.location.city}, ${data.location.region}`;
+        timeDisplay.innerText = data.location.timezone;
+        ispDisplay.innerText = data.isp;
+
+        const { lat, lng } = data.location;
+        map.setView([lat, lng], 13);
+        marker.setLatLng([lat, lng]);
+    }catch (error) {
+        console.error('Error:', error);
+        alert('Something went wrong!');
+    }
+}
+
+searchBtn.addEventListener('click', () => {
+    const ipInput = searchInp.value;
+    if(ipInput !== ''){
+        searchIP(ipInput);
+    }
+});
+
+searchInp.addEventListener('keypress', (e) => {
+    const ipInput = searchInp.value;
+    if(e.key === 'Enter'){
+        searchIP(ipInput);
+    }
+});
